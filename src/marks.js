9679bcccff66f73f7509cbc820f1572eb8df418a
Diego FINOCCHIARO <diegofino15@gmail.com>
2023-10-28 14:18:30 +0200

    Fixed bug

--- src/marks.js
+++ src/marks.js
@@ -14,9 +14,8 @@ async function marks({ token, id }) {
   } ;
   }
 
-  const firebaseCollectionID = token.split("-")[0];
+  const firebaseCollectionID = parseInt(token.split("-")[0]);
   const firebaseCollection = firebaseCollections[firebaseCollectionID];
-  console.log(firebaseCollection);
 
   const firebaseDocument = token.split("-")[1];
   console.log(`MARKS - Parsing firebase data for ${firebaseDocument} in ${firebaseCollection} for ID ${id}...`)
