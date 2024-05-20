181f13414424cd56c2beb2fe93457e29346cce0c
Diego Finocchiaro <diegofino15@gmail.com>
2024-05-20 15:36:06 +0200

    a

--- src/firebaseUtils.js
+++ src/firebaseUtils.js
@@ -8,7 +8,7 @@ if (!firebase.apps.length) {
         "type": "service_account",
         "project_id": "moyennesed-v3",
         "private_key_id": process.env.PRIVATE_KEY_ID,
-        "private_key": process.env.PRIVATE_KEY,
+        "private_key": process.env.PRIVATE_KEY.replace("\\n\\", "\n"),
         "client_email": process.env.CLIENT_EMAIL,
         "client_id": process.env.CLIENT_ID,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
